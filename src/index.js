52d0347a7c35750b1d083995b191af69f10ec390
Carl <carl@carlossus.com>
2015-06-15 21:30:35 +0800

    Removing typescript. Using ES6 js. Bumped patch

--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
-var io = require("socket.io-client");
+import io from "socket.io-client";
+
 var api = {
     sub: subscribe,
     setHost: setHost,
     start: start
 };
-var socketClient;
-// Default URL 
+
+var socketClient = null;
+
+// Default URL
 var socketHost = "http://localhost";
+
 function start() {
     connect();
     return api;
 }
+
 function setHost(hostName, port) {
     socketHost = "http://" + hostName + (port ? port : 80);
     return api;
 }
+
 function connect() {
     socketClient = io(socketHost);
 }
+
 function subscribe(context, event, key, callback) {
     var subChannel = [
         (context ? context : "*"),
         (event ? event : "*"),
         (key ? key : "*")
     ].join("/");
+
     var authToken = "someToken"; // use jwt here
     var opts = {
         channel: subChannel,
         token: authToken
     };
+
     socketClient.emit("subscribe", JSON.stringify(opts));
     socketClient.on(subChannel, callback);
     return api;
 }
+
 module.exports = api;
