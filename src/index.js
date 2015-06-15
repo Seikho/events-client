56c540de4c423d6532891b43102f2d8c4e0c6d6d
Carl <carl@carlossus.com>
2015-06-15 21:25:54 +0800

    Reverted to es5/commonjs and bumped patch version

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-define(["require", "exports", "socket.io-client"], function (require, exports, io) {
-    var api = {
-        sub: subscribe,
-        setHost: setHost,
-        start: start
-    };
-    var socketClient;
-    // Default URL 
-    var socketHost = "http://localhost";
-    function start() {
-        connect();
-        return api;
-    }
-    function setHost(hostName, port) {
-        socketHost = "http://" + hostName + (port ? port : 80);
-        return api;
-    }
-    function connect() {
-        socketClient = io(socketHost);
-    }
-    function subscribe(context, event, key, callback) {
-        var subChannel = [
-            (context ? context : "*"),
-            (event ? event : "*"),
-            (key ? key : "*")
-        ].join("/");
-        var authToken = "someToken"; // use jwt here
-        var opts = {
-            channel: subChannel,
-            token: authToken
-        };
-        socketClient.emit("subscribe", JSON.stringify(opts));
-        socketClient.on(subChannel, callback);
-        return api;
-    }
+var io = require("socket.io-client");
+var api = {
+    sub: subscribe,
+    setHost: setHost,
+    start: start
+};
+var socketClient;
+// Default URL 
+var socketHost = "http://localhost";
+function start() {
+    connect();
     return api;
-});
+}
+function setHost(hostName, port) {
+    socketHost = "http://" + hostName + (port ? port : 80);
+    return api;
+}
+function connect() {
+    socketClient = io(socketHost);
+}
+function subscribe(context, event, key, callback) {
+    var subChannel = [
+        (context ? context : "*"),
+        (event ? event : "*"),
+        (key ? key : "*")
+    ].join("/");
+    var authToken = "someToken"; // use jwt here
+    var opts = {
+        channel: subChannel,
+        token: authToken
+    };
+    socketClient.emit("subscribe", JSON.stringify(opts));
+    socketClient.on(subChannel, callback);
+    return api;
+}
+module.exports = api;
